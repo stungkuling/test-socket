920c375ea86e4153356b37f57560b9231945ad13
stungkuling <princemichael.fandino@gmail.com>
2018-07-20 20:43:29 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import SocketServer, os, socket
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+        
+HOST, PORT = os.environ["OPENSHIFT_PYTHON_IP"], int(os.environ["OPENSHIFT_PYTHON_PORT"])
+
+print("HELLO WORLD | ", HOST, " -> ", PORT)
+
+# Create the server, binding to localhost.
+server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+# Activate the server; this will keep running until you
+# interrupt the program with Ctrl-C
+server.serve_forever()
