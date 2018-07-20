18e13b832160366417132cebf55dc8f212657387
stungkuling <princemichael.fandino@gmail.com>
2018-07-20 13:55:52 +0800

    Update wsgi.py

--- django-ex-master/wsgi.py
+++ django-ex-master/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
+'''
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +14,31 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
+'''
+import SocketServer, os, socket
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+        
+HOST, PORT = socket.gethostbyname(socket.gethostname()), 9999
+
+# Create the server, binding to localhost on port 9999
+server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+# Activate the server; this will keep running until you
+# interrupt the program with Ctrl-C
+server.serve_forever()
